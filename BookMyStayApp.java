9d2370d27239cccf72ff004b62be3a8e779969bf
Shambhavi Seth <sethshambhavi45@gamil.com>
2026-03-24 15:13:38 +0530

    Shambhavi  added UC1

---
+++ BookMyStayApp.java
@@ -0,0 +1,16 @@
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("     Welcome to Book My Stay App      ");
+        System.out.println("======================================");
+
+        System.out.println("Application: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+
+        System.out.println("Thank you for using the application!");
+    }
+}
+
